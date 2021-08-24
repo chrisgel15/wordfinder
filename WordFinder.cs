deecd3c17a7f311348d7af64a1974d60ac296ac3
Christian Gelman <christiangelman@gmail.com>
2021-08-23 23:56:08 -0300

    WordFinder implementation

---
+++ WordFinder.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class WordFinder
+{
+	private WordMatrix _wordMatrix;
+	public WordFinder(IEnumerable<string> matrix)
+	{
+		int sideLenght = CalculateSideLenght(matrix);
+		this._wordMatrix = new WordMatrix(matrix, sideLenght);
+	}
+	public IEnumerable<string> Find(IEnumerable<string> wordStream)
+	{
+		return _wordMatrix.FindTopTenResults(wordStream);
+	}
+	private int CalculateSideLenght(IEnumerable<string> matrix)
+	{
+		int totalLength = 0;
+		foreach (string s in matrix)
+		{
+			totalLength++;
+		}
+
+		double sideLenght = Math.Sqrt(totalLength);
+
+		ValidateMatrixIsAPerfectSquare(sideLenght);
+
+		return (int)sideLenght;
+	}
+	private void ValidateMatrixIsAPerfectSquare(double sideLenght)
+	{
+		if (sideLenght % 1 != 0)
+			throw new Exception("Input matrix is not a perfect square.");
+	}
+}
